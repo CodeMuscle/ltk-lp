264c74d26e78d68719f02638c977106eb5ed1cb5
CodeMuscle <investportal99@gmail.com>
2024-11-10 22:37:32 +0530

    Story, Testimonials, Responsiveness

--- src/app/(components)/Navbar.tsx
+++ src/app/(components)/Navbar.tsx
@@ -6,13 +6,19 @@ import Image from "next/image";
 import searchIcon from "../../../public/assets/search.svg";
 import profileIcon from "../../../public/assets/profile.svg";
 import cartIcon from "../../../public/assets/cart.svg";
+import logo from "../../../public/assets/logo.svg";
 
 const Navbar = () => {
   return (
     <nav className="bg-white w-full py-8 flex flex-row lg:justify-end justify-center items-center">
-      <div className="flex max-w-7xl gap-x-16 items-center justify-between">
+      <div className="flex max-w-[90%] w-full lg:max-w-7xl gap-x-16 items-center justify-between">
+        {/* Logo */}
+        <div className="logo hidden lg:block">
+          <Image src={logo} alt="ltk-logo" width={100} height={100} />
+        </div>
+
         {/* Navlinks */}
-        <div className="flex flex-row items-center justify-center lg:gap-x-16 gap-x-6 text-black">
+        <div className="hidden lg:flex flex-row items-center lg:justify-center justify-between lg:gap-x-16 gap-x-6 text-black">
           {navLinks.map((link) => (
             <Link
               key={link.id}
@@ -23,6 +29,12 @@ const Navbar = () => {
             </Link>
           ))}
         </div>
+
+        {/* Mobile View */}
+        <div className="logo lg:hidden">
+          <Image src={logo} alt="ltk-logo" width={80} height={80} />
+        </div>
+
         <div className="lg:pl-16 flex flex-row items-center justify-center gap-x-8 text-black lg:mr-12 xl:mr-28">
           <button className="font-normal lg:text-lg text-base">
             <Image src={searchIcon} alt={`Search`} height={25} width={25} />
--- src/app/(components)/OfferBanner.tsx
+++ src/app/(components)/OfferBanner.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const OfferBanner = () => {
   return (
     <div className="w-full py-4 flex items-center justify-center bg-[#84160A]">
-      <p className="lg:text-base hidden text-white font-normal">
+      <p className="lg:block text-base hidden text-white font-normal">
         Get 20% OFF on your first order! Use coupon code “WELCOME” *T&C apply
       </p>
       <p className="lg:hidden text-base text-white font-normal px-6 lg:px-0 text-center">
