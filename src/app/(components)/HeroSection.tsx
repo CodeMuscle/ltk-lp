b3211811a3f373bc08403d560c8bc6d79688d64d
CodeMuscle <investportal99@gmail.com>
2024-11-17 18:20:26 +0530

    Updated Hero Section MAxW

--- src/app/(components)/HeroSection.tsx
+++ src/app/(components)/HeroSection.tsx
@@ -5,7 +5,7 @@ import heroBanner from "../../../public/assets/heroBanner.png";
 
 const HeroSection = () => {
   return (
-    <section className="w-full max-w-screen-2xl mx-auto">
+    <section className="w-full max-w-7xl mx-auto">
       <Image src={heroBanner} alt="heroBanner" style={{ width: "100%" }} />
     </section>
   );
