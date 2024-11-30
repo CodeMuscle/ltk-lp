083fdde3c6e0aa0d0a138b8429d306f16c9ad66f
CodeMuscle <investportal99@gmail.com>
2024-11-30 16:47:03 +0530

    Fixed type definitions

--- src/types/types.ts
+++ src/types/types.ts
@@ -5,7 +5,7 @@ export type Product = {
   image_url: string | null;
   discount_price: number;
   mrp: number;
-  quantity: string;
+  quantity: string[];
   weight: number;
   available: boolean;
   created_at: string;
